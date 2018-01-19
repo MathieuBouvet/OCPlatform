4f28f527f00eef29d05982ddb50a7bb8d3514630
MathieuBouvet <mathieubouvet42@gmail.com>
2018-01-19 11:40:13 +0100

    Project Creation

---
+++ README.md
@@ -0,0 +1,4 @@
+Symfony
+=======
+
+A Symfony project created on January 19, 2018, 8:39 am.
---
+++ src/OC/PlateformBundle/Controller/AdvertController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace OC\PlateformBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class AdvertController extends Controller{
+	public function IndexAction(){
+		$content = $this
+			->get('templating')
+			->render('OCPlateformBundle:Advert:index.html.twig',array('nom' => 'Moi'));
+		return new Response($content);
+	}
+
+	public function ByeAction(){
+		$content = $this
+			->get('templating')
+			->render('OCPlateformBundle:ADvert:bye.html.twig');
+		return new Response($content);
+	}
+}
