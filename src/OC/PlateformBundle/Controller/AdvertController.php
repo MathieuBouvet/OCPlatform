4026b2191e60d5bae5d6d66c934615cfc5469e5c
MathieuBouvet <mathieubouvet42@gmail.com>
2018-01-25 09:27:24 +0100

    Edition de la route oc_plateform_home pour avoir un comportement par page

--- src/OC/PlateformBundle/Controller/AdvertController.php
+++ src/OC/PlateformBundle/Controller/AdvertController.php
@@ -7,11 +7,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 
 class AdvertController extends Controller{
-	public function IndexAction(){
+	public function IndexAction($page){
 		$content = $this
 			->get('templating')
 			->render('OCPlateformBundle:Advert:index.html.twig',array('nom' => 'Moi'));
-		return new Response($content);
+		return new Response($content."<div> La page : ".$page." </div>");
 	}
 
 	public function viewAction($id){
@@ -23,5 +23,7 @@ class AdvertController extends Controller{
 
 		return new Response("<body>On afficherait l'annonce datant de l'année ".$year.", ayant le nom ".$slug.".".$_format."</body>");
 	}
-	
+	plublic function etgethethrthrthrt(){
+		
+	}
 }
