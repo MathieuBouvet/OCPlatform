1367524046d98294137bf10f54a3bd78e111f55e
MathieuBouvet <mathieubouvet42@gmail.com>
2018-02-01 07:37:07 +0100

    Modification de l'indexAction pour lancer une exception si l'id de la page est inferieure à 1

--- src/OC/PlateformBundle/Controller/AdvertController.php
+++ src/OC/PlateformBundle/Controller/AdvertController.php
@@ -4,15 +4,21 @@ namespace OC\PlateformBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 
 class AdvertController extends Controller{
+
 	public function IndexAction($page){
-		$content = $this
-			->get('templating')
-			->render('OCPlateformBundle:Advert:index.html.twig',array('nom' => 'Moi'));
-		return new Response($content."<div> La page : ".$page." </div>");
+		if($page < 1){
+			throw new NotFoundHttpException('Page '.$page." inexistante");
+		}else{
+			$content = $this
+				->get('templating')
+				->render('OCPlateformBundle:Advert:index.html.twig',array('nom' => 'Moi'));
+			return new Response($content."<div> La page : ".$page." </div>");
+		}
 	}
 
 	public function viewAction($id, Request $request){
