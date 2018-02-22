24c3da83f304947f0962de87c4b57e3469f91090
MathieuBouvet <mathieubouvet42@gmail.com>
2018-02-22 10:40:38 +0100

    Ajout d'une page d'accueil à l'application qui affiche les 3 dernières annonces

--- src/CoreBundle/Controller/DefaultController.php
+++ src/CoreBundle/Controller/DefaultController.php
@@ -8,6 +8,41 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('CoreBundle:Default:index.html.twig');
+    	/**
+    	 * Données de tests, normalement récupérées en BDD...
+    	 * @var array
+    	 */
+    	$listAdvert = array(
+    		array(
+    			'title' => "Recherche développpeur Symfony",
+    			'id' => "1",
+    			'author' => "Alexandre",
+    			'content' => "Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…'",
+    			'date' => new \DateTime()
+    		),
+    		array(
+    			'title' => "Mission de webmaster",
+    			'id' => "2",
+    			'author' => "Hugo",
+    			'content' => "Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…",
+    			'date' => new \DateTime()
+    		),
+    		array(
+    			'title' => "Offre de stage webdesigner",
+    			'id' => "3",
+    			'author' => "Mathieu",
+    			'content' => "Nous proposons un poste pour webdesigner. Blabla…",
+    			'date' => new \DateTime()
+    		),
+    		array(
+    			'title' => "Recherche clés",
+    			'id' => "4",
+    			'author' => "Mathieu",
+    			'content' => "Nous cherchons désespérement les clés du mystère au chocolat, bien qu'elle soient probablement dans le chocolat et donc inaxessibles.",
+    			'date' => new \DateTime()
+    		)
+    	);
+    	//$listAdvert = array(); // Cas d'une liste vide
+        return $this->render('CoreBundle:Default:index.html.twig',array('adverts'=>$listAdvert));
     }
 }
